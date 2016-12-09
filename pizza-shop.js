7494da8dcf3ca853ada70b3b5228532f90d46027
Josh <jalexy12@gmail.com>
2016-12-09 10:51:38 -0500

    Added pizza shop

---
+++ pizza-shop.js
@@ -0,0 +1,50 @@
+class PizzaShop {
+  constructor(name){
+      this.name            = name;
+      this.orders          = [];
+      this.processingOrder = false;
+  }
+
+  takeOrder(person, foods){
+    this.orders.push({ person: person, foods: foods });
+    console.log(`Taking order for ${person.name}`)
+    this._cook();
+  }
+
+  _cook(){
+    if (!this.processingOrder){
+      this.processingOrder = true;
+      let orderInterval = setInterval(()=> {
+        if (this.orders.length > 0){
+          let order = this.orders.shift();
+
+          console.log(`Cooking order for ${order.person.name}`);
+          order.foods.forEach( food => console.log(`Cooking ${food}`));
+
+          this.processingOrder = false
+        } else {
+          clearInterval(orderInterval);
+          this.processingOrder = false;
+          return;
+        }
+
+      }, 1000)
+    }
+  }
+}
+
+class Person {
+  constructor(name){
+    this.name = name;
+  }
+}
+
+const myShop     = new PizzaShop("Ironhack Pizza");
+const ironhacker1 = new Person("Ironhacker 1");
+const ironhacker2 = new Person("Ironhacker 2");
+const ironhacker3 = new Person("Ironhacker 3");
+const ironhacker4 = new Person("Ironhacker 4");
+
+myShop.takeOrder(ironhacker1, ["Pizza"]);
+myShop.takeOrder(ironhacker2, ["Pizza, Ice Cream"]);
+myShop.takeOrder(ironhacker3, ["Pasta", "Salad"]);
